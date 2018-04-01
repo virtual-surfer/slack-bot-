0141d1c960e99b9b1af2e223d269ff4e638cb008
virtual-surfer <jinko.chinoxxx@gmail.com>
2018-04-01 14:01:59 +0900

    環境変数読み取り方法変更

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ import tweepy
 def search_tweet(message, word):
     message.send('CONSUMER_KEYとか探す...')
     # twitterのアクセス情報
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = tweepy.OAuthHandler(os.environ.get(CONSUMER_KEY), os.environ.get(CONSUMER_SECRET))
+    auth.set_access_token(os.environ.get(ACCESS_TOKEN), os.environ.get(ACCESS_TOKEN_SECRET))
     api = tweepy.API(auth)
     global post_text
     message.send(word + 'でtweet検索するね...')
