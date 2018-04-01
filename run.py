e6769df16b60e01e9d906f6c06e6d59d96c95aa5
virtual-surfer <jinko.chinoxxx@gmail.com>
2018-04-01 14:45:10 +0900

    いいね数評価

--- run.py
+++ run.py
@@ -5,21 +5,31 @@ import re
 import tweepy
 import os
 
+
 @respond_to('searchTweet (.*)')
 def search_tweet(message, word):
-    message.send('CONSUMER_KEYとか探す...')
     # twitterのアクセス情報
     auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
     auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
     api = tweepy.API(auth)
     post_text = ""
-    message.send(word + 'でtweet検索するね...')
-    search_results = api.search(q=word, lang='ja', result_type='recent', count=3)
+    message.send('「' + word + '」でtweet検索するね...')
+    search_results = api.search(q=word, lang='ja', result_type='recent', count=100)
+    # 検索結果を辞書{検索結果, いいね数}に詰めていく
+    result_dictionary = {}
     for result in search_results:
-        message.send('tweet検索結果見つかったからまとめる...')
-        user_name_formatted = result.user.name + "@(" + result.user.screen_name + ")"
-        text = result.text
+        result_dictionary.setdefault(result, result.favorite_count)
+    # いいね数が多い(valueの降順)ものからユーザー情報とつぶやき文章を取得
+    loop_count = 0
+    for k, v in sorted(result_dictionary.items(), key=lambda x: -x[1]):
+        loop_count += 1
+        user_name_formatted = v.user.name + "@(" + v.user.screen_name + ")"
+        text = v.text
+        post_text = post_text + "-------------------------------------------------------"
         post_text = post_text + user_name_formatted + "\n" + text + "\n\n"
+        # 3個分のツイートが取れたらループ中断
+        if loop_count >= 3:
+            break
     message.send('こんなツイート見つかりましたわ。\n' + post_text)
 
 
