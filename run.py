eab9d5df41f9343309bb65835469243b9a05c431
virtual-surfer <jinko.chinoxxx@gmail.com>
2018-04-01 15:39:44 +0900

    statusから取得しておくように変更13

--- run.py
+++ run.py
@@ -22,6 +22,9 @@ def search_tweet(message, word):
         tweet_link = 'https://twitter.com/' + user.screen_name + '/status/' + str(result.id)
         result_text = '\n' + user.name + '@(' + user.screen_name + ')\n' + result.text + '\n(' + str(favorite_count) + 'いいね)' +tweet_link + '\n'
         result_dictionary.setdefault(result_text, result.favorite_count)
+    if len(result_dictionary) == 0:
+        message.send('ツイート見つからなかったす。')
+        return
     # いいね数が多い(valueの降順)ものからユーザー情報とつぶやき文章を取得
     loop_count = 0
     for k, v in sorted(result_dictionary.items(), key=lambda x: -x[1]):
