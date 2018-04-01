ec78a6003f02f640dd508955e41045870ad39374
virtual-surfer <jinko.chinoxxx@gmail.com>
2018-04-01 13:31:36 +0900

    debug追加

--- run.py
+++ run.py
@@ -9,15 +9,19 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+
 @respond_to('searchTweet (.*)')
 def search_tweet(message, word):
+    message.send(word + 'でtweet検索するね...')
     global post_text
     search_results = api.search(q=word, lang='ja', result_type='recent', count=3)
     for result in search_results:
+        message.send('tweet検索結果見つかったからまとめる...')
         user_name_formatted = result.user.name + "@(" + result.user.screen_name + ")"
         text = result.text
         post_text = post_text + user_name_formatted + "\n" + text + "\n\n"
     message.send('こんなツイート見つかりましたわ。\n' + post_text)
+
 
 def main():
     bot = Bot()
