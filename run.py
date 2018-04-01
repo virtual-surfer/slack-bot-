623fc391f21e641c05a4c94158eef0ce0f62d983
virtual-surfer <jinko.chinoxxx@gmail.com>
2018-04-01 20:36:38 +0900

    screenshot撮影してpost19

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def coingecko_screenshot(message):
     driver.save_screenshot(filename)
 
     output = BytesIO()
-    img = Image.open(filename).convert('RGB').tobytes()
+    img = Image.open(filename).tobytes()
     image_obj = Image.open(BytesIO(img), 'r')
     image_obj.save(output, 'jpeg')
 
