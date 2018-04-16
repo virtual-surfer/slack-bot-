137cfa8dfd0255d1b601721d9a977259ef5b890a
Ryo Saito <ryo.saito.k@gmail.com>
2018-04-16 17:10:47 +0900

    herokuでmysql接続設定

--- repository/common_repository.py
+++ repository/common_repository.py
@@ -3,16 +3,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# 環境変数からDB接続情報取得
-MYSQL_USER = os.environ['MYSQL_USER']
-MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
-MYSQL_HOST = os.environ['MYSQL_HOST']
-MYSQL_DB = os.environ['MYSQL_DB']
+# LocalのMySQL接続
+# MYSQL_USER = os.environ['MYSQL_USER']
+# MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+# MYSQL_HOST = os.environ['MYSQL_HOST']
+# MYSQL_DB = os.environ['MYSQL_DB']
+# engine = create_engine('mysql://{user}:{password}@{host}/{db}'
+#                        .format(user=MYSQL_USER, password=MYSQL_PASSWORD, host=MYSQL_HOST, db=MYSQL_DB),
+#                        encoding='utf8mb4', echo=False)
 
-# MySQL接続
-engine = create_engine('mysql://{user}:{password}@{host}/{db}'
-                       .format(user=MYSQL_USER, password=MYSQL_PASSWORD, host=MYSQL_HOST, db=MYSQL_DB),
-                       encoding='utf-8', echo=False)
+# HerokuのMySQL接続
+engine = create_engine(os.environ['CLEARDB_DATABASE_URL'], encoding='utf8mb4', echo=False)
 
 
 def create_session():
