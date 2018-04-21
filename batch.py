fda10d1a79fc4c8c2130ffd8f3f219a8138e453d
Ryo Saito <ryo.saito.k@gmail.com>
2018-04-21 13:20:33 +0900

    自動制御するアカウント増やす

---
+++ batch.py
@@ -0,0 +1,5 @@
+import lcai_batch
+import virtual_surfer_batch
+
+virtual_surfer_batch.execute()
+lcai_batch.execute()
--- virtual_surfer_batch.py
+++ virtual_surfer_batch.py
@@ -10,23 +10,20 @@ from service import timeutil_service
 # 本に関するつぶやき : 2時間ごと
 # アンフォロー : 2日に1回 18時
 # フォロー : 2日に1回 20時
-# 安い本の紹介 : 2時間ごと
-# 新着本の紹介 : 2時間ごと
-# メンション返信(最適な本のレコメンド) : 適宜(TODO:1日の上限は設ける)
+# メンション=いいね/リツイート=いいね or リツイートする : (TODO:1日の上限は設ける)
 
 # ##################################################################
 
 # ################## 各種変数(基本的にはここだけ触る) ##################
 
-user_screen_name = 'lcaichan18'
-max_follow_count_every_other_day = 100
-max_unfollow_count_every_other_day = 100
-follow_target_word_list = ['読書', 'ビジネス書', '本好き', 'Amazon', 'タイムセール', 'ブログ', 'Kindle', 'Amazonプライム',
-                           '漫画', 'プログラミング', '本読む', 'book', '読書好き', 'Webデザイン', 'プログラミング初心者',
-                           'Webエンジニア', '図書館', '世田谷', '下北沢', 'ITベンチャー', 'startup', '起業', '投資',
-                           'デザイン', 'はてなブログ', 'Python', 'Ruby', 'Go', 'Java', 'Javascript', 'Node.js'
-                           'React', 'Angular', 'AR', 'VR', '吉祥寺', '左利きのエレン', 'note', '女子高生社長'
-                           '女子高生エンジニア', '女子高生デザイナー', '女子高生プログラマー']
+user_screen_name = 'virtual_techX'
+max_follow_count_every_other_day = 15
+max_unfollow_count_every_other_day = 15
+follow_target_word_list = ['プログラミング', 'プログラミング初心者', 'Webエンジニア', 'ITベンチャー', 'startup', '起業', '投資',
+                           'デザイン', 'はてなブログ', 'Python', 'Ruby', 'Go', 'Java', 'Javascript', 'Node.js', 'React',
+                           'Angular', 'AR', 'VR', '機械学習', 'deep learning', 'MR', 'china tech', 'スタートアップ', '仮想通貨',
+                           'MONA', 'monapy', 'もにゃ', 'xrp', 'XEM', 'NEM', 'ブロックチェーン', 'ETH', 'Ethereum', 'QSP',
+                           'TRX', 'トロン', 'crypt currency', '仮想現実', 'Ripple', 'BTC', 'BCH', 'ビットコイン']
 
 # ##################################################################
 
@@ -55,11 +52,11 @@ def execute():
         # twitter_service.search_unpopular_tweet_and_tweet(user_screen_name, random.choice(follow_target_word_list))
     # 奇数時間につぶやき
     # done 適当にツイート検索して引用リツイート
-    # TODO: 話題の本に関してのつぶやきの引用リツイート
+    # TODO: 話題のつぶやきの引用リツイート
     if current_hour % 2 != 0 and current_hour != 19:
         twitter_service.search_popular_tweet_and_retweet(user_screen_name, random.choice(follow_target_word_list))
     # 指定時間にセール情報と新着情報
-    if current_hour == 11 or current_hour  == 15 or current_hour == 19:
+    if current_hour == 11 or current_hour == 15 or current_hour == 19:
         print('{} TODO3 current_hour:{}'.format(user_screen_name, current_hour))
 
     today = timeutil_service.current_datetime().day
